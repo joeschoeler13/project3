6bde62df1eb72162f3232259b14871f244077159
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-11-20 20:57:35 +0100

    Initial commit of files from previous project3 (on github project3_alt), added vcap-local and installed npm.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 
 /*
- * GET home page.
+ * Get all pages
  */
+
+exports.aboutus = function(req, res){
+  res.render('aboutus.html', { title: 'ARCSolutions - About Us' });
+};
+
+exports.convinced = function(req, res){
+  res.render('convinced.html', { title: 'ARCSolutions - Contact Us' });
+};
 
 exports.index = function(req, res){
   res.render('index.html', { title: 'Cloudant Boiler Plate' });
+};
+
+exports.process = function(req, res){
+  res.render('process.html', { title: 'ARCSolutions - A Project Process' });
+};
+
+exports.references = function(req, res){
+  res.render('references.html', { title: 'ARCSolutions - Our References' });
+};
+
+exports.welcome = function(req, res){
+  res.render('welcome.html', { title: 'ARCSolutions - Welcome' });
 };
