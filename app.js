263ebaa61ff2735bc178e5b7c85897a5d50dd76e
Joe Schoeler <31747663+joeschoeler13@users.noreply.github.com>
2017-11-21 03:21:53 +0100

    Added basic auth to all pages.

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var express = require('express'),
     path = require('path'),
     fs = require('fs');
 
+// Authentication module.
+var auth = require('http-auth');
+var basic = auth.basic({
+	realm: "auth-db",
+	file: __dirname + "/public/data/passwords"
+});
+
 var app = express();
 
 var db;
@@ -39,8 +46,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/style', express.static(path.join(__dirname, '/views/style')));
+app.use(auth.connect(basic));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
